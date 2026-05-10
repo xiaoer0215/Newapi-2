9c29c4a35bba39b3e4baf32494fcc215151ce3d3
xiaoer0215 <113199679+xiaoer0215@users.noreply.github.com>
2026-05-10 13:46:35 +0800

    由于落后多个版本同步更新,不建议用户部署。版本为自用

--- controller/group.go
+++ controller/group.go
@@ -12,14 +12,10 @@ import (
 )
 
 func GetGroups(c *gin.Context) {
-	groupNames := make([]string, 0)
-	for groupName := range ratio_setting.GetGroupRatioCopy() {
-		groupNames = append(groupNames, groupName)
-	}
 	c.JSON(http.StatusOK, gin.H{
 		"success": true,
 		"message": "",
-		"data":    groupNames,
+		"data":    ratio_setting.GetAllGroupNames(),
 	})
 }
 
